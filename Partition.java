bad049a76dfb6e6ff4eff53f9c0adb5d39c3909b
wangshihao <2250323804@qq.com>
2025-11-14 15:50:19 +0800

    1

--- Partition.java
+++ Partition.java
@@ -3,11 +3,17 @@ import java.util.*;
 
 
 public class Partition {
-    static List<String> partition(){
-        List<String> ans = new ArrayList<>();
+    static List<List<String>> partition(String s){
+        List<List<String>> ans = new ArrayList<>();
         return ans;
     }
+
+    static boolean isPalindrome(){
+
+        return true;
+    }
     public static void main(String[] args) {
-        
+        List<List<String>> ans = partition("aab");
+        System.out.println(ans);
     }
 }
